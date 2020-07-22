6cfc567620cdcece12349abfa43874090f3970cb
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-07-22 10:45:16 -0400

    Add files via upload

---
+++ uninstall-npm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo rm -rf /usr/local/lib/node
+sudo rm -rf /usr/local/lib/node-debug
+sudo rm -rf /usr/local/lib/node-gyp
+sudo rm -rf /usr/local/lib/node_modules
+sudo rm -rf /usr/local/include/node
+sudo rm -rf /usr/local/share/man/man1/node*
+sudo rm -rf /usr/local/share/man/man1/npm*
+sudo rm -rf /usr/local/lib/dtrace/node.d
+sudo rm -rf /usr/local/share/doc/node
+sudo rm -rf /usr/local/share/systemtap/tapset/node.stp
+sudo rm -rf /usr/local/bin/node
+sudo rm -rf /usr/local/bin/npm
+sudo rm -rf /Users/$USER/.npm
+sudo rm -rf /Users/$USER/.node-gyp
+sudo rm -rf /Users/$USER/.node_repl_history
